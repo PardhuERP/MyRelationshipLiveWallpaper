2119ab160338d044c94bf855f939c60fd445ae0a
PardhuERP <251159763+PardhuERP@users.noreply.github.com>
2026-09-22 10:48:39 +0530

    Remove unused import for RectF in MyLiveWallpaperService

--- app/src/main/java/com/myrelationship/livewallpaper/MyLiveWallpaperService.kt
+++ app/src/main/java/com/myrelationship/livewallpaper/MyLiveWallpaperService.kt
@@ -4,7 +4,6 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
-import android.graphics.RectF
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
 import java.text.SimpleDateFormat
