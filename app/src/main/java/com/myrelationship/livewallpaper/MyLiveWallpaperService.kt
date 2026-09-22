a84c27e8043b626d0853dd63daac2a8df95ad3ad
PardhuERP <251159763+PardhuERP@users.noreply.github.com>
2026-09-22 16:57:24 +0530

    Change text from 'Look down to unlock' to 'Hear beat...'

--- app/src/main/java/com/myrelationship/livewallpaper/MyLiveWallpaperService.kt
+++ app/src/main/java/com/myrelationship/livewallpaper/MyLiveWallpaperService.kt
@@ -1260,7 +1260,7 @@ if (isAnniversaryToday(anniversary)) {
 
             drawText(
                 canvas,
-                "Look down to unlock",
+                "Hear beat...",
                 centerX,
                 170f,
                 18f,
