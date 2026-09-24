318e52022df8ba77a92f79a4ef863c84bad77485
PardhuERP <251159763+PardhuERP@users.noreply.github.com>
2026-09-24 09:16:11 +0530

    Change sleep duration to 1000ms in MyLiveWallpaperService
    Increased sleep duration in wallpaper service to 1000ms to reduce CPU usage.

--- app/src/main/java/com/myrelationship/livewallpaper/MyLiveWallpaperService.kt
+++ app/src/main/java/com/myrelationship/livewallpaper/MyLiveWallpaperService.kt
@@ -476,7 +476,7 @@ private fun drawPetal(
 
                         drawWallpaper()
 
-                        Thread.sleep(40)
+                        Thread.sleep(1000)
 
                     } catch (_: InterruptedException) {
 
@@ -488,7 +488,7 @@ private fun drawPetal(
                         // crashing because of a drawing error.
 
                         try {
-                            Thread.sleep(200)
+                            Thread.sleep(1000)
                         } catch (_: Exception) {
                             break
                         }
